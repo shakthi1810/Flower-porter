53ccc68d5807f68cce4fd8dd26e48025bc1fa244
shakthi1810 <shaktheeswaran@186851@gmail.com>
2024-03-09 21:06:48 +0530

    Navigation updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Flower-Porter';
+  isNavCollapsed:boolean = false;
+
+  isSideNavToggled(event:boolean){
+    this.isNavCollapsed = event;
+    console.log(this.isNavCollapsed)
+  }
 }
